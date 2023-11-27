5b0b585d186ab0c8419717d8fa6de7ad4865d6b9
mouafo <hari.randoll@gmail.com>
2023-11-27 20:46:52 +0100

    mediaquerry section

--- src/components/reusable/BaseContainer.vue
+++ src/components/reusable/BaseContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="p-3 p-lg-5">
+  <section class="p-3 p-lg-5 mobile">
     <div class="container">
       <div class="row">
         <h2 class="my-md-5 mb-3 text-center">{{ title }}</h2>
@@ -13,4 +13,15 @@ export default {
   props: ["title"],
 };
 </script>
-<style></style>
+<style>
+@media (max-width: 770px) {
+  .mobile {
+    margin-top: 300px;
+  }
+}
+@media (max-width: 400px) {
+  .mobile {
+    margin-top: 400px;
+  }
+}
+</style>
